01799c5e7096c73e2d3f27d739c5e0ce0991e404
Florina-MadalinaTrif <155907664+Florina-MadalinaTrif@users.noreply.github.com>
2025-02-01 17:23:53 +0200

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, POST, OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
